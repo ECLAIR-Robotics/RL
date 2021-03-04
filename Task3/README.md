3a524816dc96622fb152efb7927bcb3ee6a4229b
sahiljain11 <sahilbot1@gmail.com>
2021-03-03 18:57:24 -0600

    Added Challenge 3!

---
+++ Task3/README.md
@@ -0,0 +1,24 @@
+# Task 2 - MDPs and TD(0)
+
+## Installation
+
+```
+$ conda install -c conda-forge gym
+$ pip install gym-gridworlds
+```
+
+You can use the following link to install gym-gridworld: [here](https://github.com/podondra/gym-gridworlds). Should work right out of the box! Credits to the original repository. Feel free to use the #halp-desk discord channel if you need assistance!
+
+![Cliff](cliff.png)
+
+## Instructions
+
+For this coding challenge, you'll be implementing Q-Learning and Expected SARSA from the most recent lecture. In this environment, each step you take will have a -1 penalty and if you fall off the side of the cliff, you will recieve a -100 penalty. If you say go up when on the top row (or right on the right most column, etc) you will remain in your current state but still receive the -1 penalty. This is very similar to the previous challenge but in this case, we're using different algorithms. **We highly reccomend that you do Q-Learning first as Expected SARSA is a bit more intense since you have to deal with probabilities of picking certain actions.**
+
+In terms of coding, we have labeled "TODO"s as to where you need to code. There should be 2 blocks of code and 2 conceptual questions we ask at the end that you should fill out. A "convert_state_tuple" function is given to you that takes the arguments from the OpenAI Gym Environment which holds a (x.y) tuple and converts taht into a 1D flattened coordinate. The bottom half-ish of the code is a way for the code to traverse the optimal path that you created starting from position 1 and going to the goal. If the code gets stuck there, it's likely your value function is getting stuck in a loop (ex: the value states to go up on a state but then the next value states to go down).
+
+You will have a peer for this assignment. If you do not have a peer (after 24 hours of the lecture have passed) or run into any issues, please reach out to one of the officers and we shall assist. Though you have a peer, please feel free to talk to other groups and discuss what others have come up with.
+
+Important note: for this assignment, there's no "grading" per se, so just try your best! You may need to tune the hyperparameters (~fancy~ word for epsilon, step size, discount rate, and other constants) in order to replicate the results, but feel free to mess around with things and no worries if you can't get things exact!
+
+Best wishes! :)))
---
+++ Task3/qlearning.py
@@ -0,0 +1,104 @@
+import gym
+import gym_gridworlds
+import numpy as np
+import random
+import math
+
+random.seed(21)
+np.random.seed(42)
+
+env = gym.make('Cliff-v0')
+env.seed(70)
+
+MAP_SIZE      = [4, 12]
+STATES        = MAP_SIZE[0] * MAP_SIZE[1]       # 70 possible states
+ACTIONS       = 4                               # Can either go up, right, down, or left from each state (0, 1, 2, 3)
+STEP_SIZE     = 0.1
+DISCOUNT_RATE = 0.9
+EPSILON       = 0.8
+action_value = np.zeros((STATES, ACTIONS))
+
+# Args:
+# - state: tuple of (x,y) coordinate
+# Returns:
+# - Flattened version so that action_value can be 2 dimensional
+def convert_state_tuple(state):
+    (x,y) = state
+    return (x * MAP_SIZE[1]) + y
+
+def epsilon_greedy(actions):
+    r = random.random()
+
+    if r < EPSILON:
+        # greedy option
+        action = np.argmax(actions)
+    else:
+        # exploratory option
+        action = np.random.randint(0, ACTIONS)
+    return action
+
+# episode is how many times something runs until our sequences reaches termination
+for episode in range(0, 500):
+
+    # reset the environment (SA___ part of "SARSA")
+    observation = env.reset()
+    state = convert_state_tuple(observation)
+    action = epsilon_greedy(action_value[state])
+
+    done = False
+
+    while not done:            
+        # __RSA part of "SARSA"
+        observation, reward, done, _ = env.step(action)
+
+        next_state = convert_state_tuple(observation)
+        next_action = epsilon_greedy(action_value[next_state])
+
+        # TODO: Implement Q-Learning here!
+        ################# YOUR CODE ################# 
+
+
+
+        ################# YOUR CODE ################# 
+
+val_map  = np.reshape(np.argmax(action_value, 1), (MAP_SIZE[0], MAP_SIZE[1]))
+game_map = np.zeros((MAP_SIZE[0], MAP_SIZE[1]))
+
+print("Optimal action for each state (0-up, 1-right, 2-down, 3-left):")
+print(np.round(val_map))
+
+# create map to make sure everything is good
+i = 3
+j = 0
+game_map[i][j] = 1
+
+done = False
+count = 2
+observation = env.reset()
+state = convert_state_tuple(observation)
+
+while not done:
+    # up, right, down, left
+    val = action_value[state]
+
+    # find the optimal policy at the given position
+    optimal = np.argmax(val)
+    observation, reward, done, _ = env.step(optimal)
+    (i, j) = observation
+    state = convert_state_tuple(observation)
+
+    game_map[i][j] = count
+    count += 1
+
+    if i == MAP_SIZE[0] - 1 and j == MAP_SIZE[1] - 1:
+        done = True
+
+print("Optimal path generated:")
+print(game_map)
+
+# Should have the game map of the following. This is the quickest policy!
+# 
+#  [ 0. 0. 0. 0. 0. 0. 0. 0.  0.  0.  0.  0.]
+#  [ 0. 0. 0. 0. 0. 0. 0. 0.  0.  0.  0.  0.]
+# [[ 2. 3. 4. 5. 6. 7. 8. 9. 10. 11. 12. 13.]
+#  [ 1. 0. 0. 0. 0. 0. 0. 0.  0.  0.  0. 14.]]
